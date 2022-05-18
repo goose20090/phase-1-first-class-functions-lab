c65aa7ab33ef7ccf0c87528645b54abea91cbad5
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-05-18 16:06:17 +0100

    commit tree 1bdecf301defededdcb0249d2979d95118593266

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function(names){
+    return names.slice(0,2);
+}
+
+const returnLastTwoDrivers= function(names){
+    return names.slice(-2);
+}
+
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function fareCalculator(fare){ return fare * multiplier};
+}
+
+const fareDoubler= createFareMultiplier(2);
+
+const fareTripler= createFareMultiplier(3);
+
+
+function selectDifferentDrivers (drivers, exampleFunction){
+    return exampleFunction(drivers);
+}
